38f91cd4ba704850159ea88f0d52121eb4c5236c
snowrushtrain <143225132+snowrushtrain@users.noreply.github.com>
2023-09-13 16:16:05 -0500

    making the response into table - RM

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,11 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# Normalizes the JSON
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Converts Data to Dataframe
+streamlit.dataframe(fruityvice_normalized)
+
 
 
 
