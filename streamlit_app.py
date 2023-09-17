d1de72c595fc08f5cdebfe8bb3b6fe6f83498866
snowrushtrain <143225132+snowrushtrain@users.noreply.github.com>
2023-09-16 21:50:41 -0500

    Correcting indent - RM

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,6 @@ def get_fruityvice_data(this_fruit_choice):
  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
  return fruityvice_normalized
  
-
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -62,8 +61,8 @@ if streamlit.button('Get Fruit Load List'):
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-  my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-  return "Thanks for adding " + new_fruit
+   my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+   return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
